77101965d9404e1a24e333ec13fe4883efd3ce4b
PoneyClairDeLune <88174309+PoneyClairDeLune@users.noreply.github.com>
2022-07-03 08:03:04 +0000

    Finalizing ESBuild build system

--- sh/build.sh
+++ sh/build.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
+echo Cleaning up previous build results...
 mkdir -p build
-# Remove the dev files
-rm -rv build/*
-# Using esbuild to build all JS files
-es
+rm -r build/*
+echo Copying static files...
+cp -r manifest.json build
+cp -r libs build
+cp -r LICENSE build
+cp -r _locales build
+cp -r ui build
+echo Building JS and CSS files...
+mkdir -p build/ui/js
+ls -1 js | while IFS= read -r jsf ; do
+	esbuild --bundle "js/${jsf}/index.js" --outfile=build/ui/js/${jsf}.js --minify
+done
+mkdir -p build/ui/css
+ls -1 js | while IFS= read -r jsf ; do
+	esbuild --bundle "css/${jsf}/index.css" --outfile=build/ui/js/${jsf}.css --minify
+done
+mkdir -p build/svc
+esbuild --bundle svc/ics.js --bundle svc/monitor.js --outdir=build/svc --minify
+echo Packing into ZIP files...
+mkdir -p dist
+rm -rfv dist/*
+cd build
+zip -r9v ../dist/minuette_mfv2.zip ./*
+cd ..
+echo Build finished.
+exit
